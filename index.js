ca08a2d383213ae6a237459418f7d7b339a90aba
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-12-05 21:09:29 +0600

    add routes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-
+const router = require('./routes/user.routes')
+const userRouter = require('./routes/user.routes')
 const PORT = process.env.PORT || 8080
 const app = express()
 
 
-app.get('/', (reg, res)=>{
-res.send("Hello")
-})
+app.use('/api', userRouter)
 app.listen(PORT, ()=> console.log(`server started in PORT ${PORT}`))
