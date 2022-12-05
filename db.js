627fb7fec73fb999e6f7db668ccd0f6879a40ce9
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-12-05 22:03:18 +0600

    add controller

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
 	password:'kuzya',
 	host:'localhost',
 	port:5432,
-	data:'user'
+	database:'user'
 })
 
 module.exports=pool
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const userRouter = require('./routes/user.routes')
 const PORT = process.env.PORT || 8080
 const app = express()
 
-
+app.use(express.json())
 app.use('/api', userRouter)
 app.listen(PORT, ()=> console.log(`server started in PORT ${PORT}`))
